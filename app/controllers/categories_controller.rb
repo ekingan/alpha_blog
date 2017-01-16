24d458fbe611a5427f8370c59843b938c674be09
ekingan <ekingan@gmail.com>
2017-01-16 10:55:54 -0800

    categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
+  before_action :set_category, only: [:update, :edit, :show, :destory]
 
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
@@ -20,12 +21,31 @@ class CategoriesController < ApplicationController
   end
 
   def show
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:success] = "Successfully updated name"
+      redirect_to category_path(@category)
+    else
+      flash[:danger] = "That didnt work"
+      redirect_to 'new'
+    end
+
   end
 
   private
 
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
   end
 
   def require_admin
